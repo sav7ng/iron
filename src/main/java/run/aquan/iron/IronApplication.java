90b25ffe07e527985486829212026804a4b98f37
853029827@qq.com <853029827@qq.com>
2019-08-16 17:15:56 +0800

    修改配置

--- src/main/java/run/aquan/iron/IronApplication.java
+++ src/main/java/run/aquan/iron/IronApplication.java
@@ -1,9 +1,15 @@
 package run.aquan.iron;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@MapperScan("com.baomidou.mybatisplus.samples.quickstart.mapper")
+@EnableScheduling
+@EnableSwagger2
 public class IronApplication {
 
     public static void main(String[] args) {
