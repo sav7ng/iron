326a69b3d89843ccf2113b4c60b4fcb48c6ef076
853029827@qq.com <853029827@qq.com>
2019-12-21 16:33:09 +0800

    GenerationType.IDENTITY

--- src/main/java/run/aquan/iron/system/model/entity/User.java
+++ src/main/java/run/aquan/iron/system/model/entity/User.java
@@ -26,7 +26,7 @@ import java.util.List;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(name = "username", columnDefinition = "varchar(100) not null comment '用户名'")
