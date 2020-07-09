1f3aa70abcf7e3a5fd50596ae478e9ad891137f8
853029827@qq.com <853029827@qq.com>
2020-07-09 18:02:16 +0800

    add ResponseBodyAdvice

---
+++ src/main/java/run/aquan/iron/system/core/CommonResultControllerAdvice.java
@@ -0,0 +1,52 @@
+package run.aquan.iron.system.core;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+/**
+ * @Class CommonResultControllerAdvice
+ * @Description TODO Controller advice for comment result.
+ * @Author Aquan
+ * @Date 2020/7/9 17:44
+ * @Version 1.0
+ **/
+@ControllerAdvice("run.aquan.iron.system.controller")
+public class CommonResultControllerAdvice implements ResponseBodyAdvice<Object> {
+
+    /**
+     * Whether this component supports the given controller method return type
+     * and the selected {@code HttpMessageConverter} type.
+     *
+     * @param returnType    the return type
+     * @param converterType the selected converter type
+     * @return {@code true} if {@link #beforeBodyWrite} should be invoked;
+     * {@code false} otherwise
+     */
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return false;
+    }
+
+    /**
+     * Invoked after an {@code HttpMessageConverter} is selected and just before
+     * its write method is invoked.
+     *
+     * @param body                  the body to be written
+     * @param returnType            the return type of the controller method
+     * @param selectedContentType   the content type selected through content negotiation
+     * @param selectedConverterType the converter type selected to write to the response
+     * @param request               the current request
+     * @param response              the current response
+     * @return the body that was passed in or a modified (possibly new) instance
+     */
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        return null;
+    }
+
+}
