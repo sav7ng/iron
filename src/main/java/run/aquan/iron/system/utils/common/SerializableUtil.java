9d8e62cff14f3d0004415594e15d527c064fdde0
853029827@qq.com <853029827@qq.com>
2020-07-20 22:30:16 +0800

    JWTAccessDeniedHandler

---
+++ src/main/java/run/aquan/iron/system/utils/common/SerializableUtil.java
@@ -0,0 +1,89 @@
+package run.aquan.iron.system.utils.common;
+
+import lombok.extern.slf4j.Slf4j;
+import run.aquan.iron.system.exception.IronException;
+
+import java.io.*;
+
+/**
+ * @Class SerializableUtil
+ * @Description TODO
+ * @Author Aquan
+ * @Date 2020.7.19 22:31
+ * @Version 1.0
+ **/
+@Slf4j
+public class SerializableUtil {
+
+    private SerializableUtil() {}
+
+    /**
+     * 序列化
+     * @param object
+     * @return byte[]
+     * @author dolyw.com
+     * @date 2018/9/4 15:14
+     */
+    public static byte[] serializable(Object object) {
+        ByteArrayOutputStream baos = null;
+        ObjectOutputStream oos = null;
+        try {
+            baos = new ByteArrayOutputStream();
+            oos = new ObjectOutputStream(baos);
+            oos.writeObject(object);
+            return baos.toByteArray();
+        } catch (IOException e) {
+            log.error("SerializableUtil工具类序列化出现IOException异常:{}", e.getMessage());
+            throw new IronException("SerializableUtil工具类序列化出现IOException异常:" + e.getMessage());
+        } finally {
+            try {
+                if (oos != null) {
+                    oos.close();
+                }
+                if (baos != null) {
+                    baos.close();
+                }
+            } catch (IOException e) {
+                log.error("SerializableUtil工具类反序列化出现IOException异常:{}", e.getMessage());
+                throw new IronException("SerializableUtil工具类反序列化出现IOException异常:" + e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * 反序列化
+     * @param bytes
+     * @return java.lang.Object
+     * @author dolyw.com
+     * @date 2018/9/4 15:14
+     */
+    public static Object unserializable(byte[] bytes) {
+        ByteArrayInputStream bais = null;
+        ObjectInputStream ois = null;
+        try {
+            bais = new ByteArrayInputStream(bytes);
+            ois = new ObjectInputStream(bais);
+            return ois.readObject();
+        } catch (ClassNotFoundException e) {
+            log.error("SerializableUtil工具类反序列化出现ClassNotFoundException异常:{}", e.getMessage());
+            throw new IronException("SerializableUtil工具类反序列化出现ClassNotFoundException异常:" + e.getMessage());
+        } catch (IOException e) {
+            log.error("SerializableUtil工具类反序列化出现IOException异常:{}", e.getMessage());
+            throw new IronException("SerializableUtil工具类反序列化出现IOException异常:" + e.getMessage());
+        } finally {
+            try {
+                if (ois != null) {
+                    ois.close();
+                }
+                if (bais != null) {
+                    bais.close();
+                }
+            } catch (IOException e) {
+                log.error("SerializableUtil工具类反序列化出现IOException异常:{}", e.getMessage());
+                throw new IronException("SerializableUtil工具类反序列化出现IOException异常:" + e.getMessage());
+            }
+        }
+    }
+
+
+}
---
+++ src/main/java/run/aquan/iron/system/utils/common/StringUtil.java
@@ -0,0 +1,114 @@
+package run.aquan.iron.system.utils.common;
+
+/**
+ * @Class StringUtil
+ * @Description TODO
+ * @Author Aquan
+ * @Date 2020.7.19 22:29
+ * @Version 1.0
+ **/
+public class StringUtil {
+
+    private StringUtil() {}
+
+    /**
+     * 定义下划线
+     */
+    private static final char UNDERLINE = '_';
+
+    /**
+     * String为空判断(不允许空格)
+     * @param str
+     * @return boolean
+     */
+    public static boolean isBlank(String str) {
+        return str == null || "".equals(str.trim());
+    }
+
+    /**
+     * String不为空判断(不允许空格)
+     * @param str
+     * @return boolean
+     */
+    public static boolean isNotBlank(String str) {
+        return !isBlank(str);
+    }
+
+    /**
+     * Byte数组为空判断
+     * @param bytes
+     * @return boolean
+     */
+    public static boolean isNull(byte[] bytes) {
+        // 根据byte数组长度为0判断
+        return bytes == null || bytes.length == 0;
+    }
+
+    /**
+     * Byte数组不为空判断
+     * @param bytes
+     * @return boolean
+     */
+    public static boolean isNotNull(byte[] bytes) {
+        return !isNull(bytes);
+    }
+
+    /**
+     * 驼峰转下划线工具
+     * @param param
+     * @return java.lang.String
+     */
+    public static String camelToUnderline(String param) {
+        if (isNotBlank(param)) {
+            int len = param.length();
+            StringBuilder sb = new StringBuilder(len);
+            for (int i = 0; i < len; i++) {
+                char c = param.charAt(i);
+                if (Character.isUpperCase(c)) {
+                    sb.append(UNDERLINE);
+                    sb.append(Character.toLowerCase(c));
+                } else {
+                    sb.append(c);
+                }
+            }
+            return sb.toString();
+        } else {
+            return "";
+        }
+    }
+
+    /**
+     * 下划线转驼峰工具
+     * @param param
+     * @return java.lang.String
+     */
+    public static String underlineToCamel(String param) {
+        if (isNotBlank(param)) {
+            int len = param.length();
+            StringBuilder sb = new StringBuilder(len);
+            for (int i = 0; i < len; i++) {
+                char c = param.charAt(i);
+                if (c == 95) {
+                    i++;
+                    if (i < len) {
+                        sb.append(Character.toUpperCase(param.charAt(i)));
+                    }
+                } else {
+                    sb.append(c);
+                }
+            }
+            return sb.toString();
+        } else {
+            return "";
+        }
+    }
+
+    /**
+     * 在字符串两周添加''
+     * @param param
+     * @return java.lang.String
+     */
+    public static String addSingleQuotes(String param) {
+        return "\'" + param + "\'";
+    }
+}
